e5ac12a7573efbdf3dac754c7a1824d0f8367f6f
Wenshao Zhong <jzhong@foxmail.com>
2019-08-30 16:06:01 -0400

    impl random io test helper

---
+++ devio.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdatomic.h>
+
+#define GRANUNITY 100
+
+struct rw_params {
+    int fd;
+    void *buf;
+
+    int read_percent;
+    __time_t duration;
+    __uint64_t block_size;
+    __uint64_t io_size;
+    __uint64_t size;
+
+    __uint64_t seed;
+
+    struct timespec *start;
+    struct timespec *stop;
+
+    atomic_uint_fast64_t read_io;
+    atomic_uint_fast64_t write_io;
+};
+
+static __thread __uint64_t __rs_u64 = 88172645463325252lu;
+
+static inline __uint64_t xorshift(__uint64_t s) {
+    s ^= s >> 12;
+    s ^= s << 25;
+    s ^= s >> 27;
+    return s;
+}
+
+static inline __uint64_t random_u64(void) {
+    __rs_u64 = xorshift(__rs_u64);
+    return __rs_u64 * 2685821657736338717lu;
+}
+
+static inline __uint64_t srandom_u64(const __uint64_t seed) {
+    __rs_u64 = seed ? seed : 88172645463325252lu;
+    return random_u64();
+}
+
+// compares two timespec struct
+// ret > 0 if t1 greater than t2, t1 is behind t2
+// ret == 0 if t1 equals to t2, t1 is exactly same as t2
+// ref < 0 if t1 smaller than t2, t1 is ahead t2
+static inline int8_t timespec_cmp(struct timespec *t1, struct timespec *t2) {
+    if (t1->tv_sec > t2->tv_sec) {
+        return 1;
+    }
+    if (t1->tv_sec < t2->tv_sec) {
+        return -1;
+    }
+    if (t1->tv_nsec > t2->tv_nsec) {
+        return 2;
+    }
+    if (t1->tv_nsec < t2->tv_nsec) {
+        return -2;
+    }
+    return 0;
+}
+
+void *rw_worker(void *args) {
+    struct rw_params *param = (struct rw_params*)args;
+    __uint64_t offset;
+    int ret;
+    struct timespec now;
+    struct timespec stop;
+    __uint64_t reads = 0;
+    __uint64_t writes = 0;
+    stop.tv_sec = param->start->tv_sec + param->duration;
+    stop.tv_nsec = param->start->tv_nsec;
+    const __uint64_t mask = ~(param->block_size - 1);
+    srandom_u64(pthread_self());
+
+    while(1) {
+        for (int i = 0; i < GRANUNITY; i++) {
+            offset = (random_u64() % param->size) & mask;
+            if (param->read_percent > (random_u64() % 100)) {
+                ret = pread(param->fd, param->buf, param->block_size, offset);
+                if (ret <= 0) {
+                    fprintf(stderr, "read error %d\n", ret);
+                    exit(1);
+                }
+                reads++;
+            } else {
+                ret = pwrite(param->fd, param->buf, param->block_size, offset);
+                if (ret <= 0) {
+                    fprintf(stderr, "write error %d\n", ret);
+                    exit(1);
+                }
+                writes++;
+            }
+        }
+
+        clock_gettime(CLOCK_MONOTONIC, &now);
+        if (timespec_cmp(&now, &stop) >= 0) {
+            break;
+        }
+    }
+
+    atomic_fetch_add(&param->read_io, reads);
+    atomic_fetch_add(&param->write_io, writes);
+
+    param->stop->tv_sec = now.tv_sec;
+    param->stop->tv_nsec = now.tv_nsec;
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 6) {
+        fprintf(stderr, "%s <dev> <nthreads> <read_percent> <blocksize_shift> <duration> ... (optional) <stonewall>\n", argv[0]);
+        exit(1);
+    }
+
+    const int fd = open(argv[1], O_RDWR | __O_DIRECT);
+    if (fd < 0) {
+        fprintf(stderr, "open %s error %d\n", argv[1], fd);
+        exit(1);
+    }
+
+    const int nthreads = atoi(argv[2]);
+    const int read_percent = atoi(argv[3]);
+    const int block_size = 1 << atoi(argv[4]);
+    const int duration = atoi(argv[5]);
+    const off_t size = lseek(fd, 0, SEEK_END);
+    struct timespec now;
+    struct timespec stop;
+    pthread_t thread_id[nthreads];
+
+    struct rw_params job_args = {
+        .fd = fd,
+        .buf = aligned_alloc(4096, block_size),
+        
+        .read_percent = read_percent,
+        .duration = duration,
+        .block_size = block_size,
+        .size = size,
+
+        .start = &now,
+        .stop = &stop,
+    };
+    clock_gettime(CLOCK_MONOTONIC, &now);
+
+    for (int i = 0; i < nthreads; i++) {
+        pthread_create(&thread_id[i], NULL, rw_worker, &job_args);
+    }
+
+    for (int i = 0; i < nthreads; i++) {
+        pthread_join(thread_id[i], NULL);
+    }
+
+    double dt = (double)(job_args.stop->tv_sec - job_args.start->tv_sec) +
+                (double)(job_args.stop->tv_nsec -  job_args.start->tv_nsec) / 1000000000.0;
+    if (job_args.read_io > 0) {
+        printf("READ:\n\tread_io: %d\n\ttime: %.3f seconds\n", job_args.read_io, dt);
+        double iops = (double)job_args.read_io / dt;
+        printf("\tIOPS: %.3f\n\tbandwidth: %.3f MiB/s\n", iops, iops * (double)block_size / (1024.0 * 1024.0));
+    }
+
+    if (job_args.write_io> 0) {
+        printf("WRITE:\n\twrite_io: %d\n\ttime: %.3f seconds\n", job_args.write_io, dt);
+        double iops = (double)job_args.write_io / dt;
+        printf("\tIOPS: %.3f\n\tbandwidth: %.3f MiB/s\n", iops, iops * (double)block_size / (1024.0 * 1024.0));
+    }
+}

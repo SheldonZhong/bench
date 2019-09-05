94146215ee969c686a5be3bc6f5e056c6ada237a
Wenshao Zhong <jzhong@foxmail.com>
2019-09-05 11:19:52 -0500

    move common static functions to common.h

--- devio.c
+++ devio.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdatomic.h>
+#include "common.h"
 
 #define GRANUNITY 100
 
@@ -25,45 +26,6 @@ struct rw_params {
     atomic_uint_fast64_t read_io;
     atomic_uint_fast64_t write_io;
 };
-
-static __thread __uint64_t __rs_u64 = 88172645463325252lu;
-
-static inline __uint64_t xorshift(__uint64_t s) {
-    s ^= s >> 12;
-    s ^= s << 25;
-    s ^= s >> 27;
-    return s;
-}
-
-static inline __uint64_t random_u64(void) {
-    __rs_u64 = xorshift(__rs_u64);
-    return __rs_u64 * 2685821657736338717lu;
-}
-
-static inline __uint64_t srandom_u64(const __uint64_t seed) {
-    __rs_u64 = seed ? seed : 88172645463325252lu;
-    return random_u64();
-}
-
-// compares two timespec struct
-// ret > 0 if t1 greater than t2, t1 is behind t2
-// ret == 0 if t1 equals to t2, t1 is exactly same as t2
-// ref < 0 if t1 smaller than t2, t1 is ahead t2
-static inline int8_t timespec_cmp(struct timespec *t1, struct timespec *t2) {
-    if (t1->tv_sec > t2->tv_sec) {
-        return 1;
-    }
-    if (t1->tv_sec < t2->tv_sec) {
-        return -1;
-    }
-    if (t1->tv_nsec > t2->tv_nsec) {
-        return 2;
-    }
-    if (t1->tv_nsec < t2->tv_nsec) {
-        return -2;
-    }
-    return 0;
-}
 
 void *rw_worker(void *args) {
     struct rw_params *param = (struct rw_params*)args;
